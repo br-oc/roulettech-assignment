bc7820019597f7cdd4ca3aa13d4fbfe7832d53d1
broc <87042812+br-oc@users.noreply.github.com>
2024-07-15 21:01:32 -0700

    Update settings.py

--- roulettech/settings.py
+++ roulettech/settings.py
@@ -127,7 +127,6 @@ STATIC_URL = '/static/'
 
 8
 
-CORS_REPLACE_HTTPS_REFERER      = False
 HOST_SCHEME                     = "http://"
 SECURE_PROXY_SSL_HEADER         = None
 SECURE_SSL_REDIRECT             = False
